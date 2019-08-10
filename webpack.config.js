b3beb55b53a1139ab83ab9df759ade7e98a1b5c5
José Miguel Sánchez Alés <sio2sio2+github@gmail.com>
2019-08-10 21:03:20 +0200

    Cambia la configuración de webpack para mejorar la legibilidad y crear un único fichero

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,117 @@
-module.exports = env => {
-   let mode;
+const webpack = require("webpack"),
+      merge = require("webpack-merge"),
+      MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+      CopyPlugin = require('copy-webpack-plugin'),
+      name = require("./package.json").name;
 
+
+// Configuración para Babel
+function confBabel(env) {
+   return {
+      module: {
+         rules: [
+            {
+               test: /\.js$/,
+               exclude: /node_modules/,
+               use: {
+                  loader: "babel-loader",
+                  options: {
+                     presets: [["@babel/env", {
+                        debug: env.debug,
+                        corejs: 3,
+                        useBuiltIns: "usage"
+                     }]]
+                  }
+               }
+            },
+         ]
+      }
+   }
+}
+
+// Configuración adicional para el sabor bundle,
+// o sea, el que contiene todas las dependencias.
+function confBundle() {
+   return {
+      entry: {
+         [name]: ["leaflet/dist/leaflet.css"]
+      },
+      module: {
+         rules: [
+            {
+               test: /\.css$/,
+               use: [MiniCssExtractPlugin.loader,
+                     "css-loader"]
+            },
+            {
+               test: /\.(png|jpe?g|gif|svg)$/i,
+               use: [
+                  'url-loader?limit=4096&name=images/[name].[ext]'
+               ]
+            }
+         ]
+      },
+      plugins: [
+         new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+            chunkFilename: "[id].css"
+         })
+      ]
+   }
+}
+
+
+// Configuración sin dependencias
+function confNoDeps() {
+   return {
+      externals: {
+         leaflet: {
+            root: "L",
+            amd: "leaflet",
+            commonjs: "leaflet",
+            commonjs2: "leaflet"
+         }
+      },
+      output: {
+         libraryTarget: "umd",
+         umdNamedDefine: true,
+         library: "Lcz"
+      }
+   }
+} 
+
+// Configuración para desarrollo
+// (los mapeos de código fuente en fichero aparte)
+function confDev(filename) {
+   return {
+      devtool: false,
+      plugins: [
+         new webpack.SourceMapDevToolPlugin({
+            filename: `${filename}.map`
+         })
+      ]
+   }
+}
+
+
+//Configuración adicional para depuración
+//(Se requiere copiar el ejemplo en el servidor)
+function confDebug() {
+   return {
+      devServer: {
+         contentBase: false,
+         open: "chromium",
+         openPage: "index.html?num=4"
+      },
+      plugins: [
+         new CopyPlugin([
+            {from: "examples", to: ".", ignore: ["*.swp"] }
+         ])
+      ]
+   }
+}
+
+module.exports = env => {
    switch(env.output) {
       case "debug":
       case "src":
@@ -10,5 +121,39 @@ module.exports = env => {
          mode = "production";
    }
 
-   return require(`./webpack/webpack.${mode}.js`)(env);
+   switch(env.output) {
+      case "min":
+         filename = "[name].js";
+         break;
+      case "debug":
+         filename = "dist/[name].js";
+         break;
+      case "src":
+         filename = "[name]-src.js";
+         break
+      default:
+         filename = `[name].${env.output}.js`;
+   }
+
+   const common = {
+      mode: mode,
+      entry: {
+         [name]: ["./src/index.js"]
+      },
+      output: {
+         filename: filename
+      },
+      plugins: [
+         new webpack.ProvidePlugin({
+            L: "leaflet"
+         })
+      ]
+   }
+
+   return merge.smart(
+      common,
+      mode === "production"?confBabel(env):confDev(filename),
+      env.output === "bundle"?confBundle():confNoDeps(),
+      env.output === "debug"?confDebug():null
+   )
 }
